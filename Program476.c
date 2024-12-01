650f9fa97df2ce4d6389cc859a79e6311de731c0
Hey-Aryan <142288080+Hey-Aryan@users.noreply.github.com>
2024-12-02 02:55:20 +0530

    Add files via upload

---
+++ Program476.c
@@ -0,0 +1,90 @@
+//                        **************
+//Reverse the linked list * PHYSICALLY *
+//                        **************
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));      
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*First == NULL) 
+    {
+        *First = newn;
+    }
+    else                
+    {
+        newn->next = *First;
+        *First = newn;
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements from the Linked List are : \n");
+
+    while(First != NULL)
+    {
+        printf("| %d |-> ",First->data);
+        First = First -> next;
+    }
+    printf("NULL \n");
+}
+
+void Reverse(PPNODE First)
+{
+    PNODE Previous, Next, Current; //3 pointer 
+
+    Previous = NULL;
+    Next = NULL;
+    Current = *First;
+
+    while(Current != NULL)
+    {
+        Next = Current->next;
+        Current->next = Previous;
+        Previous = Current;
+
+        Current = Next;
+    }//                 --------------
+    *First = Previous; // Due to this | <-------
+}//                     --------------         |
+//                                             |
+int main() //                                  |
+{//                                            |
+    PNODE Head = NULL;//                       |
+//                                             |
+    InsertFirst(&Head, 101);//                 |
+    InsertFirst(&Head, 51);//                  |
+    InsertFirst(&Head, 21);//                  |
+    InsertFirst(&Head, 11);//                  |
+//                                             |
+    Display(Head);//                           |
+//                         --------------------------------------- 
+    Reverse(&Head);//      |This is the beauty of call by address| 
+//                         ---------------------------------------                    
+    Display(Head); //         
+
+    Reverse(&Head);
+
+    Display(Head);
+
+    return 0;
+}
